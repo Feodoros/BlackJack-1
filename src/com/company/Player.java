bc423de16286a4dce41014063d576ac58fb3b62b
Feodoros <fzhilkin@mail.ru>
2016-11-18 17:26:37 +0300

    18.11.2016 Сделали комманду, описали начало игры, член, пенис, будем описывать работу интеллекту по принятию решения

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -4,4 +4,16 @@ package com.company;
  * Created by student1 on 14.11.16.
  */
 public abstract class Player  {
+    public Hand hand = new Hand();
+
+    public void take(Card current) {
+        hand.add(current);
+
+
+    }
+
+    public Command decision() {
+        return intellect.decide();
+
+    }
 }
